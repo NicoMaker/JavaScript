5cd8e79877f29d4bee74498794840226d53dfcc3
NicoMaker <nicola.marano02gmail,com>
2025-03-06 09:53:17 +0100

    code

--- Lezioni/4_Lezione/Esercizi/1_Esercizio/Esempi.js
+++ Lezioni/4_Lezione/Esercizi/1_Esercizio/Esempi.js
@@ -3,3 +3,28 @@ function somma(a, b) {
 }
 
 const Sommaarrow = (a, b) => a + b;
+
+function ConcatenaStinga(str1, str2) {
+  return `${str1} ${str2}`;
+}
+
+const ConcatenaStingaarrow = (str1, str2) => `${str1} ${str2}`;
+
+// Anonymus function
+const Moltiplica = function (a, b) {
+  return a * b;
+};
+
+// Anonymus Function Arrow
+const MoltiplicaArroww = (a, b) => a * b,
+  Getrettangolo = (largezza, altezza) => largezza * altezza;
+
+function Hello(name = "Mario") {
+  return `Ciao ${name}`;
+}
+
+function StmpaIndirizzo(via, città) {
+  return `abito in via ${via} della città di ${città}`;
+}
+
+const Benvenuto = (nome) => `Ciao ${nome}`;
